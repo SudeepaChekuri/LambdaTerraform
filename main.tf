5ab994f0141238f50592304172eefa9eab97d0e0
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-09 23:42:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 data "archive_file" "zip_the_python_code" {
  type        = "zip"
- source_dir  = "${path.module}/python/"
- output_path = "${path.module}/python/lambda_function.zip"
+ source_dir  = "${path.module}/"
+ output_path = "${path.module}/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "example_lambda" {
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "example_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = var.lambda_handler
   runtime          = var.lambda_runtime
-  filename         = "${path.module}/python/lambda_function.py" 
+  filename         = "${path.module}/lambda_function.py" 
   source_code_hash = filebase64sha256("lambda_function.py")
 }
 
