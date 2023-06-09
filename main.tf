b03aad77cbbc3be3025e6266c1f241a9d30ab6a6
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-09 23:33:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "archive_file" "zip_the_python_code" {
+ type        = "zip"
+ source_dir  = "${path.module}/python/"
+ output_path = "${path.module}/python/lambda_function.zip"
+}
+
 resource "aws_lambda_function" "example_lambda" {
   function_name    = var.lambda_function_name
   role             = aws_iam_role.lambda_role.arn
