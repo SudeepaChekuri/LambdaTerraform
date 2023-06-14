6322b606720d091b4bc70cd35eabcf3623e7e7f2
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-14 22:41:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,28 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "archive_file" "zip_the_python_code" {
- type        = "zip"
- source_dir  = "${path.module}/"
- output_path = "${path.module}/lambda_function.zip"
+resource "lambda_s3_bucket" "example_bucket" {
+  bucket = "lambda_s3_bucket"
+  # Add other configuration options for the bucket, if needed
 }
+
+#data "archive_file" "zip_the_python_code" {
+ #type        = "zip"
+ #source_dir  = "${path.module}/"
+ #output_path = "${path.module}/lambda_function.zip"
+#}
 
 resource "aws_lambda_function" "example_lambda" {
   function_name    = var.lambda_function_name
   role             = aws_iam_role.lambda_role.arn
   handler          = var.lambda_handler
   runtime          = var.lambda_runtime
-  filename         = "${path.module}/lambda_function.zip" 
+  #filename         = "${path.module}/lambda_function.zip" 
   #source_code_hash = filebase64sha256("lambda_function.py")
 
  # Use S3 bucket as the deployment package source
-  s3_bucket        = var.lambda_s3_bucket
-  s3_key           = var.lambda_s3_key
+  s3_bucket        = lambda_s3_bucket.example_bucket.bucket
+  s3_key           = "lambda_function.zip"
   
 }
 
